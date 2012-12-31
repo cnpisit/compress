7127fb32396bd3b0df013b8955e15e5c9d9a1ed2
Liratanak <annliratanak@gmail.com>
2012-12-31 12:48:49 +0700

    Main class add look and feel

--- Compress/src/main/Main.java
+++ Compress/src/main/Main.java
@@ -1,13 +1,39 @@
 package main;
 
+import gui.CompresserFrame;
+
+import javax.swing.SwingUtilities;
+
 public class Main {
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+//                    new FunctionFrame("Function Analysis") ;
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(main.Main.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(main.Main.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(main.Main.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(main.Main.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+		
+		SwingUtilities.invokeLater(new Runnable() {	
+			@Override
+			public void run() {
+				new CompresserFrame("Compresser") ;
+			}
+		}) ;
 	}
 
 }
